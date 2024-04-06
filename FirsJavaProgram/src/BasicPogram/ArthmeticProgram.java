568ed67366b94485596e3145fec0bec850cf0c88
kaviya-vj <kaviya20venkat@gmail.com>
2024-04-06 11:06:05 +0530

    Second Commit

---
+++ FirsJavaProgram/src/BasicPogram/ArthmeticProgram.java
@@ -0,0 +1,15 @@
+package BasicPogram;
+
+public class ArthmeticProgram {
+
+	public static void main(String[] args) {
+
+               int a = 10;
+               int b = 20;
+               int sum = a+b;
+               int sub = a-b;
+System.out.println("Addition of 2 Number is" +sum);
+System.out.println("Subraction of 2 Number is" +sub);
+	}
+
+}
---
+++ FirsJavaProgram/src/BasicPogram/MultiplationProgram.java
@@ -0,0 +1,13 @@
+package BasicPogram;
+
+public class MultiplationProgram {
+
+	public static void main(String[] args) {
+		
+		int a= 10;
+		int b= 5;
+		int mult = a*b;
+		System.out.println("Multipilication o 2 number is "+mult);
+	}
+
+}
